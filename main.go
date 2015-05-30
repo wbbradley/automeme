c977fe90346ddc948abb5bedc75d00df8763db98
Will Bradley <wbbradley@uber.com>
2015-05-30 07:32:55 +0000

    some incremental improvements

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 var (
@@ -27,12 +28,26 @@ type Response struct {
 	data       string
 }
 
+func getRemoteIP(req *http.Request) string {
+	forwarded_for := req.Header.Get("x-forwarded-for")
+	ips := strings.Split(forwarded_for, ", ")
+	var userip string
+	if len(ips) > 0 && len(ips[0]) > 0 {
+		userip = ips[0]
+	} else {
+		userip = req.RemoteAddr
+	}
+	log.Println("getRemoteIP found", userip)
+	return userip
+}
+
 // Handle existence queries
 func queryServerGet(w http.ResponseWriter, req *http.Request) {
 	query := req.FormValue("q")
 	fmt.Println("querying for", query)
 	w.Header().Set("Content-Type", "application/json")
 	responses := make(chan Response)
+	userip := getRemoteIP(req)
 
 	for j := 0; j < 2; j++ {
 		go func(j int) {
@@ -40,9 +55,11 @@ func queryServerGet(w http.ResponseWriter, req *http.Request) {
 			var url = "https://ajax.googleapis.com" +
 				"/ajax/services/search/images" +
 				"?v=1.0&as_filetype=gif&q=" + url.QueryEscape(query) +
+				"&as_sitesearch=giphy.com" +
+				"&safe=off" +
+				"&userip=" + url.QueryEscape(userip) +
 				"&start=" + fmt.Sprint(j*4)
 			resp, err := http.Get(url)
-			fmt.Println("status code for twitter is", resp.StatusCode)
 			if err != nil {
 				response.statusCode = resp.StatusCode
 				responses <- response
--- static/core.js
+++ static/core.js
@@ -1,11 +1,31 @@
 ;$(function () {
+    function syntaxHighlight(json) {
+        if (typeof json != 'string') {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
   function findAnImage(searchString, startOffset) {
     var url = "/q?q=" + encodeURIComponent(searchString);
 
     $.get(url, function(data, textStatus, jqXHR) {
       var unescapedUrl, imageDiv
-
-      console.dir(data);
+      // document.getElementById("response").innerHTML = syntaxHighlight(data)
       for (j = 0; j < data.length; j++) {
         var results = data[j].responseData.results;
         if (results && results.length > 0) {
@@ -15,17 +35,21 @@
                 (j + 1).toString() + "-" +
                 (i + 1).toString());
             if (imageDiv) {
-              imageDiv.style.backgroundImage = "url(" + unescapedUrl + ")";
+              imageDiv.innerHTML = "<img src=\"" + unescapedUrl + "\" title=\"" + unescapedUrl + "\">";
             }
+          }
+        } else {
+          for (i = 0; i < 4; i++) {
+              imageDiv = document.getElementById("image-" +
+                      (j + 1).toString() + "-" +
+                      (i + 1).toString());
+              if (imageDiv) {
+                  imageDiv.innerHTML = "";
+              }
           }
         }
       }
     }, "json");
-    /*
-       if result?.content?
-       results = (JSON.parse result.content)?.responseData?.results
-       if results and results.length > 0
-       */
   }
 
   function performSearch() {
